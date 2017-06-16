136d8003c56ec8596552c83c8c251d2702409cb8
Greg Kempe <greg@kempe.net>
2017-06-16 20:32:51 +0200

    initial commit

---
+++ html_to_akn.xsl
@@ -0,0 +1,83 @@
+<?xml version="1.0"?>
+<xsl:stylesheet xmlns:xsl="http://www.w3.org/1999/XSL/Transform" version="1.0"
+  xmlns:html="http://www.w3.org/1999/xhtml"
+  xmlns="http://www.akomantoso.org/2.0"
+  exclude-result-prefixes="html">
+
+  <xsl:output method="xml" />
+
+  <xsl:template match="html:ul | html:li">
+    <xsl:element name="{local-name(.)}">
+      <xsl:apply-templates select="node()" />
+    </xsl:element>
+  </xsl:template>
+
+  <!-- tables -->
+
+  <xsl:template match="html:table">
+    <table>
+      <xsl:apply-templates select="@id | //html:tr" />
+    </table>
+  </xsl:template>
+
+  <xsl:template match="html:tr">
+    <tr>
+      <xsl:apply-templates select="node()" />
+    </tr>
+  </xsl:template>
+
+  <xsl:template match="html:td | html:th">
+    <xsl:element name="{local-name(.)}">
+      <xsl:apply-templates select="@colspan | @rowspan" />
+
+      <p>
+        <xsl:for-each select="node()">
+          <xsl:choose>
+            <xsl:when test="name(.) = 'p'">
+              <!-- ignore p (we already have one) and do children -->
+              <xsl:apply-templates select="node()"/>
+            </xsl:when>
+
+            <xsl:otherwise>
+              <xsl:apply-templates select="."/>
+            </xsl:otherwise>
+          </xsl:choose>
+        </xsl:for-each>
+      </p>
+
+    </xsl:element>
+  </xsl:template>
+
+  <!-- elements -->
+
+  <xsl:template match="html:p | html:div">
+    <p>
+      <xsl:apply-templates />
+    </p>
+  </xsl:template>
+
+  <xsl:template match="html:br">
+    <eol/>
+  </xsl:template>
+
+  <!-- attributes -->
+
+  <xsl:template match="@id | @colspan | @rowspan">
+    <xsl:attribute name="{name(.)}"><xsl:value-of select="." /></xsl:attribute>
+  </xsl:template>
+
+  <!-- text -->
+
+  <xsl:template match="text()">
+    <xsl:value-of select="."/>
+  </xsl:template>
+
+  <!-- general matching -->
+
+  <xsl:template match="*">
+    <xsl:copy>
+      <xsl:apply-templates select="@* | node()"/>
+    </xsl:copy>
+  </xsl:template>
+
+</xsl:stylesheet>
---
+++ transform.py
@@ -0,0 +1,200 @@
+# coding: utf-8
+
+import lxml.etree as ET
+from lxml.html import html5parser
+from lxml import objectify
+import re
+
+import roman
+import cobalt
+
+
+def tag(elem):
+    tagname = elem.tag
+    if '}' not in tagname:
+        return None
+    return tagname[tagname.index("}") + 1:]
+
+
+def format_num(level, n):
+    return '(%s)' % level_num(level, n)
+
+
+def level_num(level, n):
+    if level == 0:
+        return int(n)
+    if level in [1, 2]:
+        return chr(ord('a') + int(n) - 1)
+    if level in [3, 4]:
+        return roman.toRoman(int(n)).lower()
+    return '?? level=%s num=%s ??' % (level, n)
+    raise ValueError("Formatting number for level %s??" % level)
+
+
+class Transformer(object):
+    num_re = re.compile('^([^\d]*)((\d+)\.?)[\s:]*(.*)', re.I)
+    ns = None
+
+    def __init__(self):
+        self.xslt = ET.XSLT(ET.parse('html_to_akn.xsl'))
+
+    def transform_all(self, fnames):
+        self.act = cobalt.Act()
+        for node in self.act.body.iterchildren():
+            self.act.body.remove(node)
+
+        for fname in fnames:
+            print fname
+            self.transform(fname)
+
+        # do remaining transforms
+        xml = self.act.to_xml()
+        xml = ET.tostring(self.xslt(ET.fromstring(xml)), pretty_print=True)
+        self.act = cobalt.Act(xml)
+
+    def transform(self, fname):
+        # sanitize into proper XML
+        text = open(fname).read()
+        html = html5parser.fromstring(text)
+        text = ET.tostring(html)
+
+        html = objectify.fromstring(text)
+        self.ns = html.nsmap[None]
+        self.nsmap = html.nsmap
+        self.maker = objectify.ElementMaker(annotate=False, namespace=self.act.namespace, nsmap=self.act.act.nsmap)
+        root = self.xpath(html, "//h:div[@id='wrapper']")[0]
+        self.main(root)
+
+    def main(self, root):
+        context = None
+
+        for elem in root.iterchildren():
+            tagname = tag(elem)
+            if not tagname:
+                continue
+
+            if tagname == 'h1':
+                num = self.num_re.match(elem.text)
+
+                if not num:
+                    # assume preamble
+                    try:
+                        self.act.act.preamble
+                        raise ValueError("Duplicate preamble?")
+                    except AttributeError:
+                        pass
+
+                    preamble = self.maker.preamble()
+                    self.act.act.insert(1, preamble)
+                    context = preamble
+                else:
+                    # normal chapter
+                    chapter = self.maker.chapter()
+                    chapter.append(self.maker.num(num.groups()[1]))
+                    chapter.append(self.maker.heading(num.groups()[3]))
+                    chapter.set('id', 'chapter-%s' % num.groups()[2])
+                    self.act.body.append(chapter)
+                    context = chapter
+
+            elif tagname == 'h2' or tagname == 'h3':
+                num = self.num_re.match(elem.text)
+                if not num:
+                    context.append(self.maker.p(elem.text))
+                else:
+                    # section
+                    section = self.maker.section()
+                    section.append(self.maker.num(num.groups()[1]))
+                    section.append(self.maker.heading(num.groups()[3]))
+                    section.set('id', 'section-%s' % num.groups()[2])
+                    chapter.append(section)
+                    context = section
+
+            else:
+                # tail and node text?
+                self.process(context, [elem], 0, context.get('id'))
+
+    def xpath(self, node, xp):
+        return node.xpath(xp, namespaces={'h': self.ns})
+
+    def gather_text(self, name, elem):
+        # gather all text content in elem and place it into context
+        context = getattr(self.maker, name)(elem.text)
+
+        for child in elem.iterchildren():
+            if tag(child) == 'br':
+                eol = self.maker.eol()
+                eol.tail = child.tail
+                context.append(eol)
+
+        return context
+
+    def process(self, context, elements, level, idprefix):
+        list_count = 0
+
+        for i, elem in enumerate(elements):
+            tagname = tag(elem)
+
+            if tagname == 'ol':
+                if level > 0:
+                    blockList = self.maker.blockList()
+                    temp_id = idprefix + ".list%s" % list_count
+                    blockList.set('id', temp_id)
+                    list_count += 1
+                    context.append(blockList)
+                    self.process(blockList, elem.iterchildren(), level + 1, temp_id)
+                else:
+                    self.process(context, elem.iterchildren(), level, idprefix)
+
+            elif tagname == 'li':
+                if level == 0:
+                    ss = self.maker.subsection()
+                    ss.append(self.maker.num(format_num(level, i + 1)))
+                    temp_id = idprefix + '.%s' % level_num(level, i + 1)
+                    ss.set('id', temp_id)
+                    context.append(ss)
+                    item = self.maker.content()
+                    ss.append(item)
+
+                else:
+                    item = self.maker.item()
+                    item.append(self.maker.num(format_num(level, i + 1)))
+                    temp_id = idprefix + '.%s' % level_num(level, i + 1)
+                    item.set('id', temp_id)
+                    context.append(item)
+
+                if elem.text:
+                    item.append(self.gather_text('p', elem))
+
+                self.process(item, elem.iterchildren(), level + 1, temp_id)
+
+            elif tagname == 'blockquote':
+                remark = self.gather_text('remark', elem.p)
+                remark.set('status', 'editorial')
+                p = self.maker.p()
+                p.append(remark)
+                context.append(p)
+
+            elif tagname in ['p', 'div', 'table', 'ul']:
+                # let the xsl handle this
+                context.append(elem)
+
+            elif tagname == 'table':
+                context.append(elem)
+
+            else:
+                raise ValueError("Unhandled tag: %s" % tagname)
+
+
+if __name__ == '__main__':
+    for lang in ['en']:
+        print("Doing %s" % lang)
+
+        parts = ['0-5-preamble.html'] + ['%02d.html' % i for i in range(1, 14)]
+        parts = ['constitution/_site/%s/%s' % (lang, p) for p in parts]
+        # TODO schedules
+
+        tr = Transformer()
+        tr.transform_all(parts)
+
+        with open('%s.xml' % lang, 'w') as f:
+            f.write(tr.act.to_xml())
